75860472b472c8e325eaa014e5138480409c8fd3
kamisaberi <kamisaberi@gmail.com>
2024-11-23 03:21:09 +0330

    first time data downloded successfully

--- 04.py
+++ 04.py
@@ -5,17 +5,26 @@ data = {}
 responses = json.load(open('207188815.json'))
 for response in responses:
     if "requestId" in response["params"].keys():
-        if response["params"]["requestId"] in data:
-            data[response["params"]["requestId"]].append(response)
+        request_id = response["params"]["requestId"]
+        method = response["method"]
+        if request_id not in data:
+            data[request_id] = {}
+
+        if method in data[request_id]:
+            data[request_id][method].append(response)
         else:
-            data[response["params"]["requestId"]] = [response]
+            data[request_id][method] = [response]
 
     # print(list(response["params"].keys()))
 
 print(list(data.keys()))
 for datum in data:
-    print(datum, *[item["params"]["type"] for item in data[datum] if "type" in item["params"]],
-          [item["method"] for item in data[datum] if "method" in item], len(data[datum]))
+    print(datum)
+    for item in data[datum]:
+        print(item, len(data[datum][item]))
+#     types = [item["params"]["type"] for item in data[datum] if "type" in item["params"]]
+#     methods = [item["method"] for item in data[datum] if "method" in item]
+#     print(datum, *types, len(methods), len(set(methods)), *methods, len(data[datum]))
     # break
     # if "params" in data[datum]:
     #     print(datum["params"])
